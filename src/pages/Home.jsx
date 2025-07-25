f870bdb01052b558211383e5cfda4f6077d61a19
Jerry Mathew <jer2ymatheww@gmail.com>
2025-07-25 16:49:34 +0530

    feat: hotel cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import HotelCard from '../components/HotelCard';
 import Navbar from '../components/Navbar';
 
 const Home = () => {
-    return(
-        <Navbar />
+
+    const [hotels, setHotels] = useState([]);
+    useEffect(() => {
+        (
+            async () => {
+                try {
+                    const { data } = await axios.get("https://travel-app-backend-lsj1.onrender.com/api/hotels")
+                    setHotels(data);
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        )()
+    }, [])
+
+
+    return (
+        <>
+            <Navbar />
+            <main className='mt-24 p-4 flex flex-wrap justify-center gap-4'>
+                {
+                    hotels.map(hotel => <HotelCard key={hotel._id} hotel={hotel} />)
+                }
+            </main>
+        </>
     )
 }
 export default Home;
