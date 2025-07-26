2b94e9d90e8169621f5ffaed253bb8d9992cae18
Jerry Mathew <jer2ymatheww@gmail.com>
2025-07-27 01:34:42 +0530

    feat: add category tiles

---
+++ src/components/Categories.jsx
@@ -0,0 +1,58 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Categories = () => {
+    const [categories, setCategories] = useState([]);
+    const [numberOfCategoriesToShow, setNumberOfCategoriesToShow] = useState(0);
+
+    const handleShowMoreRightClick = () => {
+        setNumberOfCategoriesToShow(prev => prev + 11);
+    }
+    const handleShowMoreLeftClick = () => {
+        setNumberOfCategoriesToShow(prev => prev - 11);
+    }
+    useEffect(() => {
+        (async () => {
+            try {
+                const { data } = await axios.get("https://travel-app-backend-lsj1.onrender.com/api/categories");
+                const categoriesToShow = data.slice(
+                    numberOfCategoriesToShow + 10 > data.length ?
+                        data.length - 10 : numberOfCategoriesToShow,
+                    numberOfCategoriesToShow > data.length ?
+                        data.length : numberOfCategoriesToShow + 10);
+                setCategories(categoriesToShow);
+            } catch (error) {
+                console.log(error);
+            }
+        })();
+    }, [numberOfCategoriesToShow]);
+
+    return (
+        <section className="fixed top-16 z-40 w-full bg-white p-4 flex gap-5 text-xs font-light text-gray-700  whitespace-nowrap">
+            {
+                numberOfCategoriesToShow >= 11 &&
+                <button className="  bg-gray-100 hover:bg-gray-200 transition shadow text-gray-700" onClick={handleShowMoreLeftClick}>
+                    <span className="material-icons-outlined">
+                        chevron_left
+                    </span>
+                </button>
+            }
+
+            {categories.map(({ category, _id }) => (
+                <span key={_id} className="cursor-pointer hover:text-blue-600 transition">
+                    {category}
+                </span>
+            ))}
+            {
+                numberOfCategoriesToShow - 11 < categories.length &&
+                <button className=" bg-gray-100 hover:bg-gray-200 transition shadow text-gray-700" onClick={handleShowMoreRightClick}>
+                    <span className="material-icons-outlined">
+                        chevron_right
+                    </span>
+                </button>
+            }
+        </section>
+    );
+};
+
+export default Categories;
